05061e3a8c2cc1c96f45e9a049b809432e93d23e
Jérémy Aubourg <24474657+Erafle@users.noreply.github.com>
2026-02-21 15:27:42 +0100

    fix: avoid passing a number that isnt an int in query params

--- src/schemas/fizz-buzz.schemas.ts
+++ src/schemas/fizz-buzz.schemas.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 export const fizzBuzzQuerySchema = z
   .object({
-    from: z.coerce.number().min(1).max(100).default(1),
-    to: z.coerce.number().min(1).max(100).default(100),
+    from: z.coerce.number().int().min(1).max(100).default(1),
+    to: z.coerce.number().int().min(1).max(100).default(100),
   })
   .refine((data) => data.to >= data.from, {
     message: "'to' must be greater than or equal to 'from'",
